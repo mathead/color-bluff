79c0be2e450571b0a46d1c7670e41a6a95934cb4
Matěj Hlaváček <matej.hl@gmail.com>
2020-01-18 14:36:36 +0100

    Basic player movement

---
+++ Character.gd
@@ -0,0 +1,15 @@
+tool
+extends RigidBody2D
+
+enum Team {UNKNOWN, RED, BLUE, YELLOW}
+
+export(Team) var team = Team.UNKNOWN
+export var colors = {
+	Team.UNKNOWN: Color("000000"),
+	Team.RED: Color("cf1717"),
+	Team.BLUE: Color("1c2fc2"),
+	Team.YELLOW: Color("d5c916"),
+}
+
+func _draw():
+	draw_circle(Vector2(0, 0), 30, colors[team])
---
+++ Player.gd
@@ -0,0 +1,17 @@
+extends "res://Character.gd"
+
+export var speed = 2000
+
+func _physics_process(delta):
+    var velocity = Vector2()  # The player's movement vector.
+    if Input.is_action_pressed("ui_right"):
+        velocity.x += 1
+    if Input.is_action_pressed("ui_left"):
+        velocity.x -= 1
+    if Input.is_action_pressed("ui_down"):
+        velocity.y += 1
+    if Input.is_action_pressed("ui_up"):
+        velocity.y -= 1
+    if velocity.length() > 0:
+        velocity = velocity.normalized() * speed * delta
+    apply_central_impulse(velocity)
