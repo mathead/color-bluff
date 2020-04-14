7fc8598f9712ef2272ceffa9c50be1712bdee93d
Matěj Hlaváček <matej.hl@gmail.com>
2020-04-14 21:08:10 +0200

    Update godot version

--- Player.gd
+++ Player.gd
@@ -5,18 +5,18 @@ export var speed = 2000
 var energy = 100.0
 
 func _physics_process(delta):
-    var velocity = Vector2()  # The player's movement vector.
-    if Input.is_action_pressed("ui_right"):
-        velocity.x += 1
-    if Input.is_action_pressed("ui_left"):
-        velocity.x -= 1
-    if Input.is_action_pressed("ui_down"):
-        velocity.y += 1
-    if Input.is_action_pressed("ui_up"):
-        velocity.y -= 1
-    if velocity.length() > 0:
-        velocity = velocity.normalized() * speed * delta
-    apply_central_impulse(velocity)
+	var velocity = Vector2()  # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed * delta
+	apply_central_impulse(velocity)
 
 func _process(delta):
 	energy -= delta
